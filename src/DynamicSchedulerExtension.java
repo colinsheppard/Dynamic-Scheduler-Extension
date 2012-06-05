a09816cb017762fbf7e340b71268c7950149b7d8
Colin <colinthegaunt@gmail.com>
2012-06-05 06:58:52 -0700

    repeat primitive added which is identical to add but takes extra arg specifying the repeat interval, also fixed mistake in go that wasn't correctly advancing tick counter (closes #8, addresses #7)

--- src/DynamicSchedulerExtension.java
+++ src/DynamicSchedulerExtension.java
@@ -35,11 +35,13 @@ extends org.nlogo.api.DefaultClassManager {
 		public Double tick = null;
 		public org.nlogo.nvm.CommandTask task = null;
 		public org.nlogo.agent.AgentSet agents = null;
+		public Double repeatInterval = null;
 
-		LogoEvent(org.nlogo.agent.AgentSet agents, CommandTask task, Double tick) {
+		LogoEvent(org.nlogo.agent.AgentSet agents, CommandTask task, Double tick, Double repeatInterval) {
 			this.agents = agents;
 			this.task = (org.nlogo.nvm.CommandTask) task;
 			this.tick = tick;
+			this.repeatInterval = repeatInterval;
 			events.put(this, nextEvent);
 			this.id = nextEvent;
 			nextEvent++;
@@ -49,6 +51,16 @@ extends org.nlogo.api.DefaultClassManager {
 			this.agents = agents;
 			this.task = (org.nlogo.nvm.CommandTask) task;
 			this.tick = tick;
+		}
+		 
+		/*
+		 * If a repeatInterval is set, this method uses it to update it's tick field and then adds itself to the
+		 * schedule argument.  The return value indicates whether the event was added to the schedule again.
+		 */
+		public Boolean reschedule(LogoSchedule schedule){
+			if(repeatInterval == null)return false;
+			this.tick = this.tick + repeatInterval;
+			return schedule.schedule.add(this);
 		}
 
 		/**
@@ -73,17 +85,15 @@ extends org.nlogo.api.DefaultClassManager {
 		}
 
 		public String dump(boolean arg0, boolean arg1, boolean arg2) {
-			return tick + ((agents==null)?"":agents.toString()) + ((task==null)?"":task.toString());
+			return tick + ((agents==null)?"":agents.toString()) + ((task==null)?"":task.toString()) + ((repeatInterval==null)?"":repeatInterval.toString());
 		}
 	}
 	private static class LogoSchedule implements org.nlogo.api.ExtensionObject {
 		private final long id;
 		LogoEventComparator comparator = (new DynamicSchedulerExtension()).new LogoEventComparator();
 		TreeSet<LogoEvent> schedule = new TreeSet<LogoEvent>(comparator);
-		Double latestTick = 0.0;
 		
-		LogoSchedule(Double ticks) {
-			latestTick = ticks;
+		LogoSchedule() {
 			schedules.put(this, nextSchedule);
 			this.id = nextSchedule;
 			nextSchedule++;
@@ -167,6 +177,8 @@ extends org.nlogo.api.DefaultClassManager {
 		primManager.addPrimitive("next", new Next());
 		// dynamic-scheduler:add
 		primManager.addPrimitive("add", new Add());
+		// dynamic-scheduler:repeat
+		primManager.addPrimitive("repeat", new Repeat());
 		// dynamic-scheduler:new
 		primManager.addPrimitive("create", new NewLogoSchedule());
 		// dynamic-scheduler:go
@@ -198,7 +210,7 @@ extends org.nlogo.api.DefaultClassManager {
 
 		public Object report(Argument args[], Context context)
 				throws ExtensionException, LogoException {
-			LogoSchedule sched = new LogoSchedule(((ExtensionContext) context).workspace().world().ticks());
+			LogoSchedule sched = new LogoSchedule();
 			return sched;
 		}
 	}
@@ -248,6 +260,44 @@ extends org.nlogo.api.DefaultClassManager {
 		}
 	}
 
+	public static class Repeat extends DefaultCommand {
+
+		public Syntax getSyntax() {
+			return Syntax.commandSyntax(new int[]{Syntax.WildcardType(),
+					Syntax.WildcardType(),
+					Syntax.WildcardType(),
+					Syntax.NumberType(),
+					Syntax.NumberType()});
+		}
+
+		public void perform(Argument args[], Context context)
+				throws ExtensionException, LogoException {
+			if(args.length<5)throw new ExtensionException("dynamic-scheduler:repeat must have 5 arguments: schedule agent tick task");
+			if (!(args[0].get() instanceof LogoSchedule)) throw new ExtensionException("dynamic-scheduler:repeat expecting a schedule as the first argument");
+			LogoSchedule sched = getScheduleFromArgument(args[0]);
+			if (!(args[1].get() instanceof Agent) && !(args[1].get() instanceof AgentSet)) throw new ExtensionException("dynamic-scheduler:repeat expecting an agent or agent set as the second argument");
+			if (!(args[2].get() instanceof CommandTask)) throw new ExtensionException("dynamic-scheduler:repeat expecting a command task as the third argument");
+			if (!args[3].get().getClass().equals(Double.class)) throw new ExtensionException("dynamic-scheduler:repeat expecting a number as the fourth argument");
+			if (args[3].getDoubleValue() < ((ExtensionContext)context).workspace().world().ticks()) throw new ExtensionException("Attempted to schedule an event for tick "+args[3].getDoubleValue()+" which is before the present 'moment' of "+((ExtensionContext)context).workspace().world().ticks());
+			if (!args[4].get().getClass().equals(Double.class)) throw new ExtensionException("dynamic-scheduler:repeat expecting a number as the fifth argument");
+			if (args[4].getDoubleValue() <= 0) throw new ExtensionException("dynamic-scheduler:repeat the repeat interval must be a positive number");
+			Double repeatInterval = args[4].getDoubleValue();
+			
+			org.nlogo.agent.AgentSet agentSet = null;
+			if (args[1].get() instanceof org.nlogo.agent.Agent){
+				org.nlogo.agent.Agent theAgent = (org.nlogo.agent.Agent)args[1].getAgent();
+				agentSet = new ArrayAgentSet(theAgent.getAgentClass(),1,false,(World) theAgent.world());
+				agentSet.add(theAgent);
+			}else{
+				agentSet = (org.nlogo.agent.AgentSet) args[1].getAgentSet();
+			}
+			if(debug)printToConsole(context,"scheduling agents: "+agentSet+" task: "+args[2].getCommandTask().toString()+" tick: "+args[3].getDoubleValue()+" repeating: "+repeatInterval);
+			LogoEvent event = (new DynamicSchedulerExtension()).new LogoEvent(agentSet,args[2].getCommandTask(),args[3].getDoubleValue(),repeatInterval);
+			sched.schedule.add(event);
+		}
+	}
+
+	
 	public static class Add extends DefaultCommand {
 
 		public Syntax getSyntax() {
@@ -275,8 +325,8 @@ extends org.nlogo.api.DefaultClassManager {
 			}else{
 				agentSet = (org.nlogo.agent.AgentSet) args[1].getAgentSet();
 			}
-			if(debug)printToConsole(context,"scheduling agents: "+agentSet+" task: "+args[2].getCommandTask().toString()+" tick: "+args[3].getDoubleValue());
-			LogoEvent event = (new DynamicSchedulerExtension()).new LogoEvent(agentSet,args[2].getCommandTask(),args[3].getDoubleValue());
+			if(debug)printToConsole(context,"scheduling agents: "+agentSet+" task: "+args[2].getCommandTask().toString()+" tick: "+args[3].getDoubleValue() );
+			LogoEvent event = (new DynamicSchedulerExtension()).new LogoEvent(agentSet,args[2].getCommandTask(),args[3].getDoubleValue(),null);
 			sched.schedule.add(event);
 		}
 	}
@@ -296,16 +346,23 @@ extends org.nlogo.api.DefaultClassManager {
 			LogoEvent event = sched.schedule.isEmpty() ? null : sched.schedule.first();
 			while(event != null){
 				if(debug)printToConsole(context,"performing event-id: "+event.id+" for agent: "+event.agents+" at tick:"+event.tick);
-				sched.latestTick = event.tick;
-				tickCounter.tick(event.tick);
+				tickCounter.tick(event.tick-tickCounter.ticks());
 				
 				Iterator iter = event.agents.iterator();
 				while(iter.hasNext()){
 					org.nlogo.nvm.Context nvmContext = new org.nlogo.nvm.Context(extcontext.nvmContext().job,iter.next(),extcontext.nvmContext().ip,extcontext.nvmContext().activation);
+					if(extcontext.nvmContext().stopping)return;
 					event.task.perform(nvmContext, emptyArgs);
+					if(nvmContext.stopping)return;
 				}
 				
+				// Remove the current event as is from the schedule
 				sched.schedule.remove(event);
+				
+				// Reschedule the event if necessary
+				event.reschedule(sched);
+				
+				// Grab the next event from the schedule
 				event = sched.schedule.isEmpty() ? null : sched.schedule.first();
 			}
 		}
